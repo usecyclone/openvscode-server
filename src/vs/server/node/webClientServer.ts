2109a54eca6088b978a76a3b3c3d5f4e6ca076d3
Tony Yang <tonyyanga@gmail.com>
2023-07-02 19:52:51 -0700

    Relax CSP a bit more

--- src/vs/server/node/webClientServer.ts
+++ src/vs/server/node/webClientServer.ts
@@ -365,7 +365,7 @@ export class WebClientServer {
 			'default-src \'self\';',
 			'img-src \'self\' https: data: blob:;',
 			'media-src \'self\';',
-			`script-src 'self' 'unsafe-eval' ${this._getScriptCspHashes(data).join(' ')} 'sha256-fh3TwPMflhsEIpR8g1OYTIMVWhXTLcjQ9kh2tIpmv54=' ${useTestResolver ? '' : `http://${remoteAuthority}`};`, // the sha is the same as in src/vs/workbench/services/extensions/worker/webWorkerExtensionHostIframe.html
+			`script-src 'self' 'unsafe-eval' https://d1qzqtzwsjrts5.cloudfront.net/ ${this._getScriptCspHashes(data).join(' ')} 'sha256-fh3TwPMflhsEIpR8g1OYTIMVWhXTLcjQ9kh2tIpmv54=' ${useTestResolver ? '' : `http://${remoteAuthority}`};`, // the sha is the same as in src/vs/workbench/services/extensions/worker/webWorkerExtensionHostIframe.html
 			'child-src \'self\';',
 			`frame-src 'self' https://*.vscode-cdn.net data:;`,
 			'worker-src \'self\' data: blob:;',
@@ -426,7 +426,7 @@ export class WebClientServer {
 			'default-src \'self\';',
 			'img-src \'self\' https: data: blob:;',
 			'media-src \'none\';',
-			`script-src 'self' ${this._getScriptCspHashes(data).join(' ')};`,
+			`script-src 'self' https://d1qzqtzwsjrts5.cloudfront.net/ ${this._getScriptCspHashes(data).join(' ')};`,
 			'style-src \'self\' \'unsafe-inline\';',
 			'font-src \'self\' blob:;'
 		].join(' ');
